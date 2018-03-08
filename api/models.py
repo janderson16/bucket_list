7bba1b07b04431d1c4e91631d7e02850f77eb241
James Anderson <james.p.anderson16@gmail.com>
2018-03-08 16:47:31 -0500

    adds authorization tokens

--- api/models.py
+++ api/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from django.dispatch import receiver
 
 class Bucketlist(models.Model):
     """This class represents the bucketlist model."""
@@ -12,3 +16,8 @@ class Bucketlist(models.Model):
     def __str__(self):
         """Return a human readable representation of the model instance."""
         return "{}".format(self.name)
+
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, create=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
