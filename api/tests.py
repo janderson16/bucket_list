fadc991e4401fbd617077412e79c5a4f707a0d95
James Anderson <james.p.anderson16@gmail.com>
2018-03-08 13:39:46 -0500

    adds post for bucketlist item

--- api/tests.py
+++ api/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
 from .models import Bucketlist
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.core.urlresolvers import reverse
+
 
 class ModelTestCase(TestCase):
     """This class defines the test suite for the bucketlist model"""
@@ -15,3 +19,17 @@ class ModelTestCase(TestCase):
         self.bucketlist.save()
         new_count = Bucketlist.objects.count()
         self.assertNotEqual(old_count, new_count)
+
+class ViewTestCase(TestCase):
+    """Test suite for API"""
+
+    def setUp(self):
+        self.client = APIClient()
+        self.bucketlist_data = {'name', 'Go to Cape Town'}
+        self.response = self.client.post(
+            reverse('create'),
+            self.bucketlist_data,
+            format="json")
+
+    def test_api_can_create_a_bucketlist(self):
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
