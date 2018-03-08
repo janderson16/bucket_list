c65cf279120a4bdb23d557e1d190c37fd73e5406
James Anderson <james.p.anderson16@gmail.com>
2018-03-08 13:14:26 -0500

    adds first model test

--- api/tests.py
+++ api/tests.py
@@ -11,7 +11,7 @@ class ModelTestCase(TestCase):
 
     def test_codel_can_create_a_bucketlist(self):
         """Test the bucketlist model can create a bucketlist."""
-        old_count = Buckelist.objects.count()
+        old_count = Bucketlist.objects.count()
         self.bucketlist.save()
-        new_count = Buckelist.objects.count()
+        new_count = Bucketlist.objects.count()
         self.assertNotEqual(old_count, new_count)
